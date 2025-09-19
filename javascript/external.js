24d84857442d6ed3bdd69484f2b3c69907d878bd
cook <theirishbagel@gmail.com>
2025-09-18 20:58:33 -0500

    Completed Lab 3

--- javascript/external.js
+++ javascript/external.js
@@ -5,4 +5,13 @@ var user_name = window.prompt("Please enter your name", "Type your name here");
 console.log("Hello, welcome" + user_name + "!");
 document.getElementById("welcomeHeader").innerText =
        "Hello, welcome " + user_name + "!";
+
+      //creates a date representing the current date and time
+      var today = new Date();
+      //take only the year part of the full date for display
+      var year = today.getFullYear();
+      //finds HTML element "copyright" to set content.
+      //Content displays text reading "copyright 2025" with a copyright symbol
+ document.getElementById("copyright").innerHTML =
+      "Copyright &copy; " + year;
 }
