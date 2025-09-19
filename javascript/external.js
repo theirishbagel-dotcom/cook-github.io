5e126bb0507f585246b0da1ce9d1a15ced924e50
cook <theirishbagel@gmail.com>
2025-09-18 19:39:20 -0500

    Saved username. Replaced the header with user prompt information

--- javascript/external.js
+++ javascript/external.js
@@ -1,5 +1,8 @@
-arr = ["Mael","Eric","Randall","James"];
-for (var i=0; i < arr.length; i++)
-{
-  document.writeln(arr[i],"<br>");
+
+// A prompt box is used to prompt users to input a value before entering a page.
+window.onload = function() {
+var user_name = window.prompt("Please enter your name", "Type your name here");
+console.log("Hello, welcome" + user_name + "!");
+document.getElementById("welcomeHeader").innerText =
+       "Hello, welcome " + user_name + "!";
 }
